b1c31c88a190e31acb6f468eef47d41505d9b74e
Erick Johnson <erick@ejjohnson.org>
2014-07-26 02:23:13 -0400

    Removes date field from Equity model.

--- equity-parent/equity-model/src/main/java/io/vos/equity/model/Equity.java
+++ equity-parent/equity-model/src/main/java/io/vos/equity/model/Equity.java
@@ -2,19 +2,15 @@ package io.vos.equity.model;
 
 import com.google.common.base.Preconditions;
 
-import java.util.Date;
-
 public class Equity implements Model {
 
   private final int id;
   private final String name;
   private final String value;
-  private final Date now;
 
   public Equity(int id, String name, String value) {
     this.id = id;
     this.name = Preconditions.checkNotNull(name);
     this.value = Preconditions.checkNotNull(value);
-    this.now = new Date();
   }
 }
